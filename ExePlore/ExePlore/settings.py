ab1c12f9db27039673bc00510d1aa0e1708aff5e
Pierre Siddall <43399998+Pierre-siddall@users.noreply.github.com>
2022-02-18 19:05:42 +0000

    A basic login functionality which redirects to a home index upon a successful login

--- ExePlore/ExePlore/settings.py
+++ ExePlore/ExePlore/settings.py
@@ -52,7 +52,7 @@ ROOT_URLCONF = 'ExePlore.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [str(BASE_DIR.joinpath('templates'))],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -115,3 +115,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/'
--- ExePlore/accounts/apps.py
+++ ExePlore/accounts/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class LoginsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'logins'
+    name = 'accounts'
