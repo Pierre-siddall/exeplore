3177e58efec3345fa77c3663d5d6f6ab25e689d3
Jenni Schofield <jennischofield02@icloud.com>
2022-02-21 13:14:12 +0000

    Created database models for users
    User and Player tables created in models.py, settings.py updated accordingly (...hopefully...)

--- ExePlore/logins/models.py
+++ ExePlore/logins/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    id = models.IntegerField(primary_key = True)
+    first_name = models.CharField(max_length = 100)
+    last_name = models.CharField(max_length = 100)
+    username = models.CharField(max_length = 100)
+    email = models.EmailField(max_length  = 100)
+    password = models.CharField(max_length = 100) 
+
+class Player(models.Model):
+    user = models.OneToOneField(User,on_delete = models.CASCADE)
+    badges = models.ForeignKey(Badges, on_delete = models.CASCADE)
+    visits = models.ForeignKey(Visits, on_delete = models.CASCADE)
+    score = models.IntegerField()
