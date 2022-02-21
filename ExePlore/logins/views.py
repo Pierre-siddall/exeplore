7d0a4c2437c42f052e414a3d59a66f7fdce3327c
Priya Shah <priya.shah2309@gmail.com>
2022-02-21 16:53:03 +0000

    Added a template for register.html and worked implemented a registration stage. Still need to join DB and Django together as well as redirecting...

--- ExePlore/logins/views.py
+++ ExePlore/logins/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import registerForm
+from django.contrib import messages
 
 # Create your views here.
+def register(request):
+    if request.method == "POST":
+        form = registerForm(request.POST)
+        if form.is_valid():
+            # login needs to happen
+            # add to database
+            messages.success(request, "Registration successful.")
+        messages.error(request, "Unsuccessful registration. Invalid information.")
+    form = registerForm()
+    return render(request=request, template_name="logins/register.html", context={"register_form": form})
+
