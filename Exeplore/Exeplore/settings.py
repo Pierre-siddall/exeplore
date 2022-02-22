42a9279c2b23a10dc728b344ea79b6f545f2e9c3
Pierre Siddall <43399998+Pierre-siddall@users.noreply.github.com>
2022-02-22 11:32:04 +0000

    The new file structure

--- Exeplore/Exeplore/settings.py
+++ Exeplore/Exeplore/settings.py
@@ -34,7 +34,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles', 'ExePlore'
+    'django.contrib.staticfiles', 'Exeplore'
 ]
 
 MIDDLEWARE = [
