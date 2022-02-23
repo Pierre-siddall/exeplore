fce3e8a452334e97e3fae01b59b168f332b48cdf
Pierre Siddall <43399998+Pierre-siddall@users.noreply.github.com>
2022-02-23 16:35:14 +0000

    Linked login page to the url

--- Exeplore/Exeplore/urls.py
+++ Exeplore/Exeplore/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
+    path('login/',include('logins.urls')),
     path('admin/', admin.site.urls),
-    path('accounts/',include('django.contrib.auth.urls'))
 ]
