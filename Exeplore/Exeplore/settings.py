75d1d460d8607262b497d19bc0534c15893331a0
Jenni Schofield <jennischofield02@icloud.com>
2022-02-23 11:32:05 +0000

    please for the love of god

--- Exeplore/Exeplore/settings.py
+++ Exeplore/Exeplore/settings.py
@@ -29,7 +29,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'logins.apps.LoginsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
