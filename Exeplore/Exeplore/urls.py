799c251bb32a5e27cedbf85c2c9b70ab01e4159a
Jenni Schofield <jennischofield02@icloud.com>
2022-02-23 16:13:49 +0000

    Linking db to django

--- Exeplore/Exeplore/urls.py
+++ Exeplore/Exeplore/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/',include('django.contrib.auth.urls')),
-    path('logins/', include('logins.urls'))
-
+    path('register/', views.register, name = 'register')
 ]
