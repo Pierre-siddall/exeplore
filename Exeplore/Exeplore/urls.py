3c88c4230cae09bef3dc18535787cb3ab35ec674
Jenni Schofield <jennischofield02@icloud.com>
2022-02-23 11:14:51 +0000

    Update urls.py

--- Exeplore/Exeplore/urls.py
+++ Exeplore/Exeplore/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('logins/', include('logins.urls')),
     path('admin/', admin.site.urls),
+    path('accounts/',include('django.contrib.auth.urls'))
 ]
