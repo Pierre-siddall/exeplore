da44d4b5a0d9b822cc749ffcf57eb548d8628d13
rhenshaw20 <roxanne.henshaw@btinternet.com>
2022-02-24 23:32:34 +0000

    FINALLY got the stylesheet to stop throwing 404 errors

--- Exeplore/Exeplore/settings.py
+++ Exeplore/Exeplore/settings.py
@@ -112,6 +112,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
