e54219709feeb9f78541d2c959f80fdebc6e3bce
Pierre Siddall <43399998+Pierre-siddall@users.noreply.github.com>
2022-02-25 15:27:26 +0000

    Changes by Priya and images to be loaded into the homepage

--- Exeplore/Exeplore/views.py
+++ Exeplore/Exeplore/views.py
@@ -4,25 +4,17 @@ from django.contrib import messages
 from django.contrib.auth import login, authenticate
 from .forms import SignUpForm
 
-
 def register(request):
     if request.method == "POST":
         form = SignUpForm(request.POST)
         if form.is_valid():
-            # login needs to happen
             form.save()
-            username = form.cleaned_data.get('username')
-            raw_password = form.cleaned_data.get('password1')  # Is this secure??
-            user = authenticate(username=username, password=raw_password)
-            login(request, user)
             messages.success(request, "Registration successful.")
-            return redirect('home') # This needs to redirect to the home page
-            # add to database
-
-        messages.error(request, "Unsuccessful registration. Invalid information.")
+            return redirect("main:home") # This needs to redirect to the home page
+        else:
+             messages.error(request, "Invalid form input")
     form = SignUpForm()
     return render(request=request, template_name="registration/register.html", context={"register_form": form})
-
 
 def login(request):
     if request.method == "POST":
