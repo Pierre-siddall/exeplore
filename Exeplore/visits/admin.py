3b6bcbb5321c2c97c79e32c806788dfa9f119756
Jenni Schofield <jennischofield02@icloud.com>
2022-03-02 16:14:13 +0000

    Added comments to all python files, conforming with linter, set up Dummy users

--- Exeplore/visits/admin.py
+++ Exeplore/visits/admin.py
@@ -1,23 +1,46 @@
+"""This file holds the admin manager classes for everything to do with
+visiting and locations"""
 from django.contrib import admin
-from .models import Location, Badge
-from users.models import Player, Earned_Badge
+from users.models import Player, EarnedBadge, Visit
 from visits.models import Badge, Location
+
 # Register your models here.
 class PlayerInLine(admin.StackedInline):
+    """This class defines how a Player should be displayed in Django's admin"""
     model = Player
     can_delete = False
     verbose_name_plural = 'players'
     list_display = ['name']
+
 class EarnedBadgeInLine(admin.StackedInline):
-    model = Earned_Badge
+    """This class defines how a EarnedBadge should be displayed in Django's admin"""
+    model = EarnedBadge
     readonly_fields = ('badge_earned_datetime', )
     list_display = ['badge',]
     can_delete = True
-    verbose_name = "Players"
+    verbose_name = "Player"
     verbose_name_plural = "Players"
 class BadgeAdmin(admin.ModelAdmin):
-    model = Badge 
-    inlines = [EarnedBadgeInLine, ]
+    """This class defines how Badges are managed, and how their many-to-many
+    relationship should be displayed in admin"""
+    model = Badge
+    inlines = [EarnedBadgeInLine,  ]
+class VisitInLine(admin.StackedInline):
+    """This class defines how a Visit should be displayed in
+    
+    Django's admin"""
+    model = Visit
+    readonly_fields = ('visit_datetime', )
+    list_display = ['visit',]
+    can_delete = True
+    verbose_name = "Player"
+    verbose_name_plural = "Players"
 
-admin.site.register(Location)
+class LocationAdmin(admin.ModelAdmin):
+    """This class defines how Locations are managed, and how their many-to-many
+    relationship should be displayed in admin"""
+    model = Location
+    inlines = [VisitInLine, ]
+
 admin.site.register(Badge, BadgeAdmin)
+admin.site.register(Location, LocationAdmin)
