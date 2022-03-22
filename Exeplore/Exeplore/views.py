a2c9da54d4a99cc6e54dc3acceed3ec389de2ce2
priyashah23 <41793470+priyashah23@users.noreply.github.com>
2022-03-22 23:31:08 +0000

    Fixes Locations
    When the markers are clicked they show the icons

--- Exeplore/Exeplore/views.py
+++ Exeplore/Exeplore/views.py
@@ -75,13 +75,15 @@ def home(request):
     user = User.objects.get(username=name)
     data = Location.objects.all()
 
+    location_names = []
     lats = []
     lngs = []
     for location in data:
         lats.append(float(location.get_lat()))
         lngs.append(float(location.get_long()))
+        location_names.append(location.get_name())
 
-    return render(request, "registration/home.html", {'user':user, 'lats':lats, 'lngs':lngs})
+    return render(request, "registration/home.html", {'user':user, 'lats':lats, 'lngs':lngs, 'lct_name': location_names})
 
 def splash(request):
     """This function renders the splash page"""
